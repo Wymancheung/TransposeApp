405df8eab02aac31b3a1f9b1e38dd8d7f38d6fde
waimancheung <wymancheung95@gmail.com>
2017-04-04 01:19:27 +0800

    Fab Updates

--- app/src/main/java/com/comp4521gp01/transposeapp/MainActivity.java
+++ app/src/main/java/com/comp4521gp01/transposeapp/MainActivity.java
@@ -1,8 +1,8 @@
 package com.comp4521gp01.transposeapp;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,12 +11,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Boolean isFabOpen = false;
     private FloatingActionButton fab,camera_fab,web_fab,gallery_fab;
+    private TextView fab_label,camera_fab_label,web_fab_label,gallery_fab_label,debug,fab_blur;
     private Animation fab_open,fab_close,rotate_forward,rotate_backward;
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +36,23 @@ public class MainActivity extends AppCompatActivity {
         fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
         rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
         rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+        fab_label = (TextView) findViewById(R.id.fab_label);
+        camera_fab_label = (TextView) findViewById(R.id.camera_fab_label);
+        web_fab_label = (TextView) findViewById(R.id.web_fab_label);
+        gallery_fab_label = (TextView) findViewById(R.id.gallery_fab_label);
+        debug = (TextView) findViewById(R.id.debug);
+        fab_blur = (TextView) findViewById(R.id.fab_blur);
+
         fab.setOnClickListener(clickListener);
         camera_fab.setOnClickListener(clickListener);
         web_fab.setOnClickListener(clickListener);
         gallery_fab.setOnClickListener(clickListener);
+        fab_blur.setOnClickListener(clickListener);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(clickListener);
+
+
     }
 
     private View.OnClickListener clickListener= new View.OnClickListener() {
@@ -44,20 +61,25 @@ public class MainActivity extends AppCompatActivity {
             int id = v.getId();
             switch (id) {
                 case R.id.fab:
-
                     animateFAB();
                     break;
                 case R.id.camera_fab:
 
-                    Log.d("Raj", "Fab 1");
+                    debug.setText("Camera");
                     break;
                 case R.id.web_fab:
 
-                    Log.d("Raj", "Fab 2");
+                    debug.setText("Web");
                     break;
                 case R.id.gallery_fab:
 
-                    Log.d("Raj", "Fab 3");
+                    debug.setText("Gallery");
+                    break;
+                case R.id.fab_blur:
+                    animateFAB();
+                    break;
+                case R.id.button:
+                    debug.setText("button");
                     break;
             }
         }
@@ -66,28 +88,45 @@ public class MainActivity extends AppCompatActivity {
     public void animateFAB(){
 
         if(isFabOpen){
+            fab_blur.setVisibility(View.INVISIBLE);
 
             fab.startAnimation(rotate_backward);
             camera_fab.startAnimation(fab_close);
             web_fab.startAnimation(fab_close);
             gallery_fab.startAnimation(fab_close);
+
+            fab_label.setVisibility(View.INVISIBLE);
+            camera_fab_label.setVisibility(View.INVISIBLE);
+            web_fab_label.setVisibility(View.INVISIBLE);
+            gallery_fab_label.setVisibility(View.INVISIBLE);
+
             camera_fab.setClickable(false);
             web_fab.setClickable(false);
             gallery_fab.setClickable(false);
+            fab_blur.setClickable(false);
             isFabOpen = false;
             Log.d("Raj", "close");
+            //dialog.dismiss();
 
         } else {
-
+            fab_blur.setVisibility(View.VISIBLE);
             fab.startAnimation(rotate_forward);
             camera_fab.startAnimation(fab_open);
             web_fab.startAnimation(fab_open);
             gallery_fab.startAnimation(fab_open);
+
+            fab_label.setVisibility(View.VISIBLE);
+            camera_fab_label.setVisibility(View.VISIBLE);
+            web_fab_label.setVisibility(View.VISIBLE);
+            gallery_fab_label.setVisibility(View.VISIBLE);
+
             camera_fab.setClickable(true);
             web_fab.setClickable(true);
             gallery_fab.setClickable(true);
+            fab_blur.setClickable(true);
             isFabOpen = true;
             Log.d("Raj","open");
+            //dialog.show();
 
         }
     }
